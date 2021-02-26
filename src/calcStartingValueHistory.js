1f442d3858b72524f39a85c230537d10f02dc3a8
Sumit Kumar <sk@outlook.com>
2021-02-26 21:25:15 +0100

    add history of realized gains and dividends

--- src/calcStartingValueHistory.js
+++ src/calcStartingValueHistory.js
@@ -1,27 +1,26 @@
-const partition = require('lodash/partition')
-const isBefore = require('date-fns/isBefore')
-const isAfter = require('date-fns/isAfter')
-const Big = require('big.js')
+const partition = require('lodash/partition');
+const sumBy = require('lodash/sumBy');
+const isAfter = require('date-fns/isAfter');
 
-const calcInventoryPurchasesFIFO = require('./calcInventoryPurchasesFIFO')
-const calcCurrentShares = require('./calcCurrentShares')
-const calcPurchasePrice = require('./calcPurchasePrice')
+const calcInventoryPurchasesFIFO = require('./calcInventoryPurchasesFIFO');
+const calcCurrentShares = require('./calcCurrentShares');
+const calcPurchasePrice = require('./calcPurchasePrice');
 
-const { applySplitMultiplier, getDateArr } = require('../utils')
+const { applySplitMultiplier, getDateArr } = require('../utils');
 
 module.exports = function (activities, interval, priceAtStart) {
   // adjust shares bought/sold by splits that happened in the past
-  activities = applySplitMultiplier(activities)
+  activities = applySplitMultiplier(activities);
 
-  const startDate = new Date(interval.start)
+  const startDate = new Date(interval.start);
   const [activitiesBeforeInterval, activitiesInInterval] = partition(
     activities,
-    a => !isAfter(new Date(a.date), startDate)
-  )
+    (a) => !isAfter(new Date(a.date), startDate),
+  );
 
-  const sharesAtStart = calcCurrentShares(activitiesBeforeInterval)
+  const sharesAtStart = calcCurrentShares(activitiesBeforeInterval);
 
-  const dateArr = getDateArr(interval)
+  const dateArr = getDateArr(interval);
 
   // this is the holding position and the beginning of the interval, wrapped as a Buy activity
   const beforeIntervalActivity = {
@@ -29,31 +28,37 @@ module.exports = function (activities, interval, priceAtStart) {
     date: interval.start,
     price: priceAtStart,
     shares: sharesAtStart,
-    amount: priceAtStart * sharesAtStart
-  }
+    amount: priceAtStart * sharesAtStart,
+  };
+
+  const realizedGainsHistory = [];
+  const dividendGainsHistory = [];
 
   const startValueHistory = dateArr.map((d, i) => {
-    const [activitiesUntilNow] = partition(
-      activitiesInInterval,
-      a => !isAfter(new Date(a.date), new Date(d))
-    )
+    const [activitiesUntilNow] = partition(activitiesInInterval, (a) => !isAfter(new Date(a.date), new Date(d)));
 
     // interval data, including the purchases before the interval as one starting purchase activity
-    const { purchases: purchasesForInterval } = calcInventoryPurchasesFIFO([
+    const { purchases: purchasesForInterval, realizedGains } = calcInventoryPurchasesFIFO([
       ...activitiesUntilNow,
-      beforeIntervalActivity
-    ])
+      beforeIntervalActivity,
+    ]);
 
     // starting value is the value of the holding at the beginning of the interval
-    const { purchaseValue: startingValue } = calcPurchasePrice(
-      purchasesForInterval
-    )
+    const { purchaseValue: startingValue } = calcPurchasePrice(purchasesForInterval);
 
-    return startingValue
-  })
+    const dividends = activitiesUntilNow.filter((a) => a.type === 'Dividend');
+    const dividendGains = sumBy(dividends, 'amount');
+
+    dividendGainsHistory.push(dividendGains);
+    realizedGainsHistory.push(realizedGains);
+
+    return startingValue;
+  });
 
   return {
     dates: dateArr,
-    startValueHistory
-  }
-}
+    startValueHistory,
+    realizedGainsHistory,
+    dividendGainsHistory,
+  };
+};
--- src/calcValueHistory.js
+++ src/calcValueHistory.js
@@ -1,57 +1,52 @@
-const partition = require('lodash/partition')
-const isBefore = require('date-fns/isBefore')
-const Big = require('big.js')
+const partition = require('lodash/partition');
+const isBefore = require('date-fns/isBefore');
+const Big = require('big.js');
 
-const calcCurrentShares = require('./calcCurrentShares')
+const calcCurrentShares = require('./calcCurrentShares');
 
-const {
-  applySplitMultiplier,
-  getDateArr,
-  normalizeQuotes
-} = require('../utils')
+const { applySplitMultiplier, getDateArr, normalizeQuotes } = require('../utils');
 
 module.exports = function (activities, quotes, interval, i) {
   if (activities.length === 0) {
     return {
       history: [],
-      dates: []
-    }
+      dates: [],
+    };
   }
 
   // adjust shares bought/sold by splits that happened in the past
-  activities = applySplitMultiplier(activities)
+  activities = applySplitMultiplier(activities);
 
-  const startDate = new Date(interval.start)
-  const [activitiesBeforeInterval, activitiesInInterval] = partition(
-    activities,
-    a => isBefore(new Date(a.date), startDate)
-  )
+  const startDate = new Date(interval.start);
+  const [activitiesBeforeInterval, activitiesInInterval] = partition(activities, (a) =>
+    isBefore(new Date(a.date), startDate),
+  );
 
-  const sharesAtStart = calcCurrentShares(activitiesBeforeInterval)
+  const sharesAtStart = calcCurrentShares(activitiesBeforeInterval);
 
   // create an array of all days from today to the first activity
-  const dateArr = getDateArr(interval)
+  const dateArr = getDateArr(interval);
 
   // get normalized quotes (so every day of dateArr has a price)
-  const quotesNormalized = normalizeQuotes(quotes, dateArr)
+  const quotesNormalized = normalizeQuotes(quotes, dateArr);
 
-  let sharesStorage = Big(sharesAtStart)
+  let sharesStorage = Big(sharesAtStart);
   const valueOfHoldingOverTime = dateArr.map((d, i) => {
-    const day = d
-    const todaysActivities = activitiesInInterval.filter(a => day === a.date)
-    const sharesDelta = calcCurrentShares(todaysActivities)
+    const day = d;
+    const todaysActivities = activitiesInInterval.filter((a) => day === a.date);
+    const sharesDelta = calcCurrentShares(todaysActivities);
 
-    const todaysShares = sharesStorage.plus(Big(sharesDelta))
-    sharesStorage = todaysShares
+    const todaysShares = sharesStorage.plus(Big(sharesDelta));
+    sharesStorage = todaysShares;
 
-    const price = quotesNormalized[i].price || 0
-    const value = +todaysShares * price
+    const price = quotesNormalized[i].price || 0;
+    const value = +todaysShares * price;
 
-    return value
-  })
+    return value;
+  });
 
   return {
     history: valueOfHoldingOverTime,
-    dates: dateArr
-  }
-}
+    dates: dateArr,
+  };
+};
