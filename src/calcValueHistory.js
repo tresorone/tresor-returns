81b2688276c6b29e823bd7dae8d4952e82d4f4fe
Sumit Kumar <sk@outlook.com>
2021-06-24 10:42:25 +0200

    feat(invested): add invested history

--- src/calcValueHistory.js
+++ src/calcValueHistory.js
@@ -1,6 +1,6 @@
+const sumBy = require('lodash/sumBy');
 const partition = require('lodash/partition');
 const isBefore = require('date-fns/isBefore');
-const Big = require('big.js');
 
 const { applySplitMultiplier, getDateArr, normalizeQuotes } = require('../utils');
 const calcInventoryPurchasesFIFO = require('./calcInventoryPurchasesFIFO');
@@ -31,6 +31,22 @@ module.exports = function (activities, quotes, interval, i) {
   // get normalized quotes (so every day of dateArr has a price)
   const quotesNormalized = normalizeQuotes(quotes, dateArr);
 
+  // calc invested capital before interval
+  const activitiesForInvestedValueBeforeInterval = activitiesBeforeInterval
+    .filter((a) => ['Sell', 'Buy'].includes(a.type))
+    .map((a) => {
+      return {
+        ...a,
+        amount: a.type === 'Sell' ? a.amount * -1 : a.amount,
+      };
+    });
+
+  // calc invested capital at the start of the interval
+  let investedStorage = sumBy(activitiesForInvestedValueBeforeInterval, 'amount');
+
+  // invested capital over time
+  const investedInHoldingOverTime = [];
+
   let sharesStorage = sharesAtStart;
   const valueOfHoldingOverTime = dateArr.map((day, i) => {
     const currentPrice = quotesNormalized[i].price;
@@ -51,11 +67,26 @@ module.exports = function (activities, quotes, interval, i) {
 
     sharesStorage = calcCurrentShares(activitiesUntilNow);
 
+    // also store the invested amount (based solely on activities) over time
+    const activitiesForInvestedValue = todaysActivities
+      .filter((a) => ['Sell', 'Buy'].includes(a.type))
+      .map((a) => {
+        return {
+          ...a,
+          amount: a.type === 'Sell' ? a.amount * -1 : a.amount,
+        };
+      });
+
+    // increase invested capital storage for next iteration and push into history
+    investedStorage += sumBy(activitiesForInvestedValue, 'amount');
+    investedInHoldingOverTime.push(investedStorage);
+
     return purchaseValue;
   });
 
   return {
     history: valueOfHoldingOverTime,
+    investedHistory: investedInHoldingOverTime,
     dates: dateArr,
   };
 };
